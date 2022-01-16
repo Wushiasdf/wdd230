9afaba4ce3a312555c9b5276952f8fea311fd0a1
Wushiasdf <u847715210@byui.edu>
2022-01-15 17:00:39 -0700

    homepage update

--- javascript/homepage-js.js
+++ javascript/homepage-js.js
@@ -1,4 +1,7 @@
 var time = document.lastModified;
+var year = 2022;
+
+document.querySelector("#year").innerHTML = year.toString();
 
 document.getElementById("update").innerHTML = time.toString();
 
